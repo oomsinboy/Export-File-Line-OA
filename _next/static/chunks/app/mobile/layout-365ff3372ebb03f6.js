f957b7101a416e7fcb22e61a8e452c0fc2cbfa0d
Payupat <payupat@planetcloud.cloud>
2024-07-30 03:36:51 +0700

    fix

---
+++ _next/static/chunks/app/mobile/layout-365ff3372ebb03f6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[705],{9194:function(n,e,_){Promise.resolve().then(_.t.bind(_,8381,23))},8381:function(n){n.exports={style:{fontFamily:"'__Kanit_e6cb70', '__Kanit_Fallback_e6cb70'",fontStyle:"normal"},className:"__className_e6cb70"}}},function(n){n.O(0,[525,971,23,744],function(){return n(n.s=9194)}),_N_E=n.O()}]);
